d62086619b882a695bca4972c866eba7a3b38906
huydd <huydangduc@bizfly.vn>
2022-06-03 17:27:22 +0700

    first commit

---
+++ src/components/MyHead.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1 class="header">My Vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyHeader',
+}
+</script>
+
+<style lang="css" scoped>
+    .header {
+        background-color: #333;
+        color: #fff;
+        padding: 10px;
+        text-align: center;
+    }
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div :class="['todo-item', todoProps.completed ? 'is-completed' : '']" :id="new_id">
+        <input type="checkbox" :checked="[todoProps.completed]">{{ todoProps.title }}
+        <button class="del-btn">Delete</button>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'TodoItem',
+    props: ['todoProps'],
+}
+</script>
+
+<style scoped>
+.todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    margin: 0;
+    border-bottom: 1px dotted #ccc;
+}
+
+.is-completed {
+    text-decoration: line-through;
+}
+
+.del-btn {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    float: right;
+}
+</style>
